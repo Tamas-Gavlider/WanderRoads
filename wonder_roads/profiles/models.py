a296739ceb66e92cbda5cd98ced0805177475dc0
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2025-01-19 13:19:48 +0000

    Register profile at admin/set database/requirements.txt

--- wonder_roads/profiles/models.py
+++ wonder_roads/profiles/models.py
@@ -1,3 +1,4 @@
+from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 from django.db import models
 from django.contrib.auth.models import User
@@ -20,6 +21,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+    
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+    
+post_save.connect(create_profile, sender=User)
--- wonder_roads/settings.py
+++ wonder_roads/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)nf6830i5zfs6qe&y7$ijt0n4esz6zove%ul(8fjy(gjm4z&g1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,11 @@ CLOUDINARY_STORAGE = {
 MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-tamasgavlid-wanderroads-evupz6meoyw.ws.codeinstitute-ide.net',
+]
+CSRF_COOKIE_SECURE = False
 
 # Application definition
 
@@ -90,12 +96,13 @@ WSGI_APPLICATION = 'wonder_roads.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
