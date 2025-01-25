7a3672b6afd86872db6852561c5bc346eddb378b
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2025-01-25 21:13:44 +0000

    Create app for countries/removed Country calls from Profile

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User 
 from cloudinary.models import CloudinaryField
-from django_countries.fields import CountryField
+from countries.models import Country
 
 # Create your models here.
 
@@ -14,12 +14,6 @@ EXPERIENCE_LEVEL = [
     ('World Voyager','World Voyager'),
     ('Global Legend','Global Legend')    
 ]
-
-class Country(models.Model):
-    name = models.CharField(max_length=100, unique=True)  
-
-    def __str__(self):
-        return self.name
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
