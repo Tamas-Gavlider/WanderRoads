8401232c10bac459e333a1a1a7c0561ffb0613de
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-01 14:31:09 +0100

    Add travel_recommendation model & serializer

--- travel_recommendation/models.py
+++ travel_recommendation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class TravelRecommendation(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recommended_destination = models.CharField(max_length=255, blank=True, null=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"Recommended destination for {self.owner} : {self.recommended_destination}"
---
+++ travel_recommendation/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import TravelRecommendation
+
+class TravelRecommendationSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    recommended_destination = serializers.ReadOnlyField()
+
+    class Meta:
+        model = TravelRecommendation
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'recommended_destination'
+        ]
+
