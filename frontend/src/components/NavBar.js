3509ae53d5d3d164e3c488abc7451ecfbba0e4ac
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-08 21:13:06 +0100

    move navlinks to the right side

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -16,7 +16,7 @@ const NavBar = () => {
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ms-auto">
             <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>Map</NavLink>
             <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>Profile</NavLink>
             <NavLink to="/posts" className={styles.NavLink} activeClassName={styles.Active}>Posts</NavLink>
