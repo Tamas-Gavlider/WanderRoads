e1f1546799a3afb2f348cd03f684add80f34ff0e
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-11 23:27:31 +0100

    update logo img/use currentusercontext to show links for logged/loggedout users

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,8 +5,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
 import {NavLink} from 'react-router-dom'
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+
 
 const NavBar = () => {
+  
+  const currentUser = useCurrentUser();
+
+  const loggedInLinks = (
+    <>
+    <NavLink exact to="/map" className={styles.NavLink} activeClassName={styles.Active}>Map</NavLink>
+            <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>Profile</NavLink>
+            <NavLink to="/posts" className={styles.NavLink} activeClassName={styles.Active}>Posts</NavLink>
+            <NavLink to="/travel-buddies" className={styles.NavLink}activeClassName={styles.Active} >Travel Buddies</NavLink>
+            <NavLink to="/logout" className={styles.NavLink} activeClassName={styles.Active}>Logout</NavLink>
+    </>
+  )
+  const loggedOutLinks = (
+    <>
+    <NavLink to="/signin" className={styles.NavLink}  activeClassName={styles.Active}>Login</NavLink>
+    <NavLink to="/signup" className={styles.NavLink}  activeClassName={styles.Active}>Register</NavLink>
+    </>
+  )
 
   return (
     <Navbar expand="lg" className={styles.NavBar} fixed="to">
@@ -17,13 +37,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink exact to="/map" className={styles.NavLink} activeClassName={styles.Active}>Map</NavLink>
-            <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>Profile</NavLink>
-            <NavLink to="/posts" className={styles.NavLink} activeClassName={styles.Active}>Posts</NavLink>
-            <NavLink to="/travel-buddies" className={styles.NavLink}activeClassName={styles.Active} >Travel Buddies</NavLink>
-            <NavLink to="/logout" className={styles.NavLink} activeClassName={styles.Active}>Logout</NavLink>
-             <NavLink to="/signin" className={styles.NavLink}  activeClassName={styles.Active}>Login</NavLink>
-            <NavLink to="/signup" className={styles.NavLink}  activeClassName={styles.Active}>Register</NavLink>
+            {currentUser ? loggedInLinks : loggedOutLinks}
           </Nav>
         </Navbar.Collapse>
       </Container>
