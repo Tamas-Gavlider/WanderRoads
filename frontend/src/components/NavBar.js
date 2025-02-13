336531f3482866b1e635279f7981e598ee796474
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-13 14:11:25 +0100

    add span around the logged in username in navlink

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/logo.png'
-import styles from '../styles/NavBar.module.css'
-import {NavLink} from 'react-router-dom'
-import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
-import axios from 'axios';
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
 
 const NavBar = () => {
-  
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
@@ -19,34 +20,80 @@ const NavBar = () => {
       await axios.post("/dj-rest-auth/logout/");
       setCurrentUser(null);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  } 
+  };
 
   const loggedInLinks = (
     <>
-    {currentUser?.username}
-    <NavLink exact to="/map" className={styles.NavLink} activeClassName={styles.Active}>Map</NavLink>
-            <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>Profile</NavLink>
-            <NavLink to="/posts" className={styles.NavLink} activeClassName={styles.Active}>Posts</NavLink>
-            <NavLink to="/travel-buddies" className={styles.NavLink}activeClassName={styles.Active} >Travel Buddies</NavLink>
-            <NavLink to="/logout" onClick={handleSignOut} className={styles.NavLink} activeClassName={styles.Active}>Logout</NavLink>
+      <NavLink
+        exact
+        to="/map"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Map
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        to="/posts"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Posts
+      </NavLink>
+      <NavLink
+        to="/travel-buddies"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Travel Buddies
+      </NavLink>
+      <NavLink
+        to="/logout"
+        onClick={handleSignOut}
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Logout
+      </NavLink>
+      <span>{currentUser?.username}</span>
     </>
-  )
+  );
   const loggedOutLinks = (
     <>
-    <NavLink to="/signin" className={styles.NavLink}  activeClassName={styles.Active}>Login</NavLink>
-    <NavLink to="/signup" className={styles.NavLink}  activeClassName={styles.Active}>Register</NavLink>
+      <NavLink
+        to="/signin"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        Register
+      </NavLink>
     </>
-  )
+  );
 
   return (
     <Navbar expand="lg" className={styles.NavBar} fixed="to">
       <Container fluid>
-        <NavLink to='/'>
-        <Navbar.Brand ><img src={logo} alt="logo" height="75"/></Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="75" />
+          </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             {currentUser ? loggedInLinks : loggedOutLinks}
@@ -54,7 +101,7 @@ const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
