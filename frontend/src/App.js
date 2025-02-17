e8756376108aaca5195a363947256d0456c9bfa6
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-17 13:18:32 +0100

    Create PostCreateForm file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import SignInForm from './pages/auth/SignInForm';
 import NavBar from './components/NavBar'
 import LandingPage from './components/LandingPage';
 import Map from './pages/map/Map';
+import PostCreateForm from './pages/posts/PostCreateForm';
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
       <Route exact path='/map' render={()=> <Map />}/>
       <Route exact path='/profiles' render={()=> <h1>Profile</h1>}/>
       <Route exact path='/posts' render={()=> <h1>Posts</h1>}/>
+      <Route exact path='/posts/create' render={()=> < PostCreateForm />}/>
       <Route exact path='/travel-buddies' render={()=> <h1>Travel Buddies</h1>}/>
       <Route exact path='/logout' render={()=> <h1>Logout</h1>}/>
       <Route render={()=> <p>Page not found!</p>}/>
