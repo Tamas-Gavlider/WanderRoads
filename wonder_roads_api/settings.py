1d7527b03c6db740686819df62879f6ce83c075d
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-21 18:23:41 +0100

    uncomment csrf trusted origins in settings.py

--- wonder_roads_api/settings.py
+++ wonder_roads_api/settings.py
@@ -64,13 +64,11 @@ DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), "localhost", "127.0.0.1"]
 
-"""
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-tamasgavlid-wanderroads-evupz6meoyw.ws.codeinstitute-ide.net' ,
-    "localhost", "127.0.0.1",
     os.environ.get('CLIENT_ORIGIN')
 ]
-"""
+
 
 # Application definition
 
