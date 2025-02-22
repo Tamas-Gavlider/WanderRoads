cc7f48218ad3c7801082e91efa6aa363a2204967
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-22 21:22:37 +0100

    update urls in post app

--- post/urls.py
+++ post/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import CountryListView, PostList, PostDetail 
 
 urlpatterns = [
-    path('posts/', PostList.as_view()),
-    path('posts/<int:pk>/', PostDetail.as_view()),
+    path('posts/', PostList.as_view(), name="post-list"),
+    path('posts/<int:pk>/', PostDetail.as_view(), name="post-detail"),
     path("countries/", CountryListView.as_view(), name="country-list"),
-
 ]
