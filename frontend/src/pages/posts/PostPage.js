e07ce3adbb4ed5e7e4839c24ac4f7eedcc1ae963
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-02-26 18:15:57 +0100

    modify path in post/comment files/create travelpreferences file in components

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -34,6 +34,7 @@ function PostPage() {
         ]);
         setPost({ results: [post] });
         setComments(comments);
+        console.log(post)
       } catch (err) {
         console.log(err);
       }
@@ -41,7 +42,9 @@ function PostPage() {
 
     handleMount();
   }, [id]);
-
+  
+  console.log(profile_image)
+  
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
