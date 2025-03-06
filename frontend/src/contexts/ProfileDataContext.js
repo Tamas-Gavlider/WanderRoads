d530e908e94bd5086c2646799681c8f81c0014a4
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-06 22:43:03 +0100

    modified profiledatacontext/friend status changed after page reload

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -3,7 +3,6 @@ import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 import { travelBuddyHelper, unFriendHelper } from "../utils/utils";
 
-
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
 
@@ -13,6 +12,7 @@ export const useSetProfileData = () => useContext(SetProfileDataContext);
 export const ProfileDataProvider = ({ children }) => {
   const [profileData, setProfileData] = useState({
     pageProfile: { results: [] },
+    allProfiles: { results: [] },
   });
 
   const currentUser = useCurrentUser();
@@ -22,12 +22,12 @@ export const ProfileDataProvider = ({ children }) => {
       console.log("Already a travel buddy.");
       return;
     }
-  
+
     try {
       const { data } = await axiosRes.post("/travel-buddy/", {
         travel_buddy: clickedProfile.id,
       });
-  
+
       setProfileData((prevState) => ({
         ...prevState,
         allProfiles: {
@@ -46,10 +46,10 @@ export const ProfileDataProvider = ({ children }) => {
       console.log("Not a travel buddy.");
       return;
     }
-  
+
     try {
       await axiosRes.delete(`/travel-buddy/${clickedProfile.travel_buddies_initiated_id}/`);
-  
+
       setProfileData((prevState) => ({
         ...prevState,
         allProfiles: {
@@ -63,18 +63,15 @@ export const ProfileDataProvider = ({ children }) => {
     }
   };
 
-
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(
-          "/profiles/"
-        );
+        const { data } = await axiosReq.get("/profiles/");
         setProfileData((prevState) => ({
           ...prevState,
           allProfiles: data,
         }));
-        console.log(data)
+        console.log(data);
       } catch (err) {
         console.log(err);
       }
@@ -82,7 +79,7 @@ export const ProfileDataProvider = ({ children }) => {
 
     handleMount();
   }, [currentUser]);
-  
+
   return (
     <ProfileDataContext.Provider value={profileData}>
       <SetProfileDataContext.Provider
