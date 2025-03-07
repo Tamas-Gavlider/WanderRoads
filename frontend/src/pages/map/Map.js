d94a3c428959e0c0cd6b76d1c70622e4659eb62e
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-07 23:39:43 +0100

    uninstall simple maps/will use different library

--- frontend/src/pages/map/Map.js
+++ frontend/src/pages/map/Map.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
-const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
 
 export default function Map() {
   return (
     <div>
-      {" "}
-      <ComposableMap>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
-            ))
-          }
-        </Geographies>
-      </ComposableMap>
+      <h1>Map</h1>
     </div>
   );
 }
