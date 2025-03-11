ab45d51ad69eeb1308d95a66c2405fa5f5ea3ed9
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-12 00:06:20 +0100

    installed simple maps/add the basic map to map page

--- frontend/src/pages/map/Map.js
+++ frontend/src/pages/map/Map.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
+
+// url to a valid topojson file
+const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
 export default function Map() {
   return (
-    <div>
-      <h1>Map</h1>
+    <div style={{ width: "100%", height: "500px" }}>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  style={{
+                    default: { fill: "#ECEFF1", stroke: "#607D8B", strokeWidth: 0.5 },
+                    hover: { fill: "#FF5722", stroke: "#000", strokeWidth: 1 },
+                    pressed: { fill: "#3F51B5", stroke: "#000", strokeWidth: 1 },
+                  }}
+                />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
     </div>
-  );
+  )
 }
