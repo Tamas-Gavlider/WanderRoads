b30bbc930b13dc4839f302c7c7c3d1e549e71f7f
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-11 18:31:50 +0100

    collect static files

---
+++ staticfiles/build/precache-manifest.7ec56ad0b38663ab4beed92a68e8ac69.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ab63c2668e8f1c58ae29f68c400789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52b30e1931c3a39c5a52",
+    "url": "/static/css/main.17c42cdb.chunk.css"
+  },
+  {
+    "revision": "901b3a4744d264d1b4d2",
+    "url": "/static/js/2.28176f25.chunk.js"
+  },
+  {
+    "revision": "a7f47eff4ea7852d00e1bc0d6c1bfdbc",
+    "url": "/static/js/2.28176f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e329b8ffcc98d45395b",
+    "url": "/static/js/3.58e34116.chunk.js"
+  },
+  {
+    "revision": "52b30e1931c3a39c5a52",
+    "url": "/static/js/main.71f2e2da.chunk.js"
+  },
+  {
+    "revision": "0d4de5da003c6e6221f6",
+    "url": "/static/js/runtime-main.984f2de9.js"
+  },
+  {
+    "revision": "c8f39d643879332561e0793b6b49fb28",
+    "url": "/static/media/landing_page.c8f39d64.webp"
+  },
+  {
+    "revision": "ba3b750f56547e27151cb29f2bb62bcc",
+    "url": "/static/media/landing_page_mobile.ba3b750f.jpg"
+  },
+  {
+    "revision": "d536d1c9f683c0ad6651c1c9479df133",
+    "url": "/static/media/logo.d536d1c9.png"
+  },
+  {
+    "revision": "75b23e4b3bfbe4f3c3804c59d90a00c5",
+    "url": "/static/media/reg_img.75b23e4b.jpg"
+  },
+  {
+    "revision": "ed50e9ba94d6da539d5d1672b4abef89",
+    "url": "/static/media/sign_in_img.ed50e9ba.jpg"
+  }
+]);
--- staticfiles/build/service-worker.js
+++ staticfiles/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17c7cfcb745f7bd39fa655679e7cb170.js"
+  "/precache-manifest.7ec56ad0b38663ab4beed92a68e8ac69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
