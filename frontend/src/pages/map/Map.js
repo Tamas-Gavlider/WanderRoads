85d34cbc57de217d15f1852b7be4ec62cbfd5aca
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-12 14:30:21 +0100

    style map/add overlaytrigger which will show the name of the country

--- frontend/src/pages/map/Map.js
+++ frontend/src/pages/map/Map.js
@@ -1,30 +1,64 @@
-import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import React, { useState } from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import styles from "../../styles/Map.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-// url to a valid topojson file
+// URL to the topojson file
 const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
 export default function Map() {
+  const [tooltipContent, setTooltipContent] = useState("");
+
   return (
-    <div style={{ width: "100%", height: "500px" }}>
-      <ComposableMap>
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  style={{
-                    default: { fill: "#ECEFF1", stroke: "#607D8B", strokeWidth: 0.5 },
-                    hover: { fill: "#FF5722", stroke: "#000", strokeWidth: 1 },
-                    pressed: { fill: "#3F51B5", stroke: "#000", strokeWidth: 1 },
-                  }}
-                />
-            ))
-          }
-        </Geographies>
+    <div>
+      <ComposableMap
+        projectionConfig={{
+          center: [0, 3],
+          scale: 200,
+        }}
+        className={styles.Map}
+      >
+        <ZoomableGroup>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                return (
+                  <OverlayTrigger
+                    key={geo.rsmKey}
+                    placement="top"
+                    overlay={<Tooltip id={`tooltip-${geo.rsmKey}`}>{geo.properties.name}</Tooltip>}
+                  >
+                    <Geography
+                      geography={geo}
+                      onMouseEnter={() => setTooltipContent(geo.properties.name)}
+                      onMouseLeave={() => setTooltipContent("")}
+                      style={{
+                        default: {
+                          fill: "#ECEFF1",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.5,
+                          outline: "none",
+                        },
+                        hover: { fill: "#00BAE2", stroke: "#000", strokeWidth: 1 },
+                        pressed: {
+                          stroke: "#000",
+                          strokeWidth: 1,
+                        },
+                      }}
+                    />
+                  </OverlayTrigger>
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
       </ComposableMap>
     </div>
-  )
+  );
 }
