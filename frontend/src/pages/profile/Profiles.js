903687fa2da93da720475f5b70785a8709b34108
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-13 22:45:32 +0100

    add searchfield to profile views/add searchbar to profiles to search by username

--- frontend/src/pages/profile/Profiles.js
+++ frontend/src/pages/profile/Profiles.js
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { Link } from "react-router-dom";
-import { Row, Col, Card, Dropdown } from "react-bootstrap";
+import { Row, Col, Card, Dropdown, Form } from "react-bootstrap";
 import ThemeSong from "../../components/ThemeSong";
 import styles from "../../styles/Profiles.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import searchStyles from '../../styles/PostsPage.module.css'
 
 const UserList = () => {
   const [profiles, setProfiles] = useState([]);
   const [sortBy, setSortBy] = useState("owner");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchProfiles = async () => {
       try {
-        const { data } = await axiosReq.get(`/profiles/?ordering=${sortBy}`);
+        const { data } = await axiosReq.get(
+          `/profiles/?ordering=${sortBy}&search=${query}`
+        );
         setProfiles(data.results);
       } catch (err) {
         console.error("Error fetching profiles:", err);
       }
     };
+    const timer = setTimeout(() => {
+      fetchProfiles();
+    }, 500);
 
-    fetchProfiles();
-  }, [sortBy]);
+    return () => clearTimeout(timer);
+  }, [sortBy, query]);
 
   return (
     <div className={styles.Users}>
+      <Col lg={4} md={6} sm={6} xs={6}>
+       <i className={`fas fa-search ${searchStyles.SearchIcon}`}/>
+      <Form className={searchStyles.SearchBar} onSubmit={(event) => event.preventDefault()}>
+        <Form.Control
+          type="text"
+          placeholder="Search users..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </Form>
+      </Col>
       <Dropdown onSelect={(eventKey) => setSortBy(eventKey)}>
         <Dropdown.Toggle id="dropdown-basic" className={btnStyles.Button}>
           Sort By: {sortBy === "owner" ? "Username" : "Posts Count"}
@@ -44,9 +62,9 @@ const UserList = () => {
               <Card.Body>
                 <Card.Title className="d-flex align-items-center gap-2">
                   <Link to={`/profiles/${user.id}`} className={styles.User}>
-                    {user.owner}'s vibe: 
+                    {user.owner}'s vibe:
                   </Link>
-                    <ThemeSong theme_song={user.theme_song} />
+                  <ThemeSong theme_song={user.theme_song} />
                 </Card.Title>
                 <Card.Text>
                   <p className={styles.Status}>{user.status}</p>
