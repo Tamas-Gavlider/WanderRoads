532a6e1a1c99e9046ce3a3e6a222d7fd8fb9ec8f
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-17 12:55:30 +0100

    add style to travel recommendations

--- frontend/src/pages/travel_recommendation/TravelRecommendation.js
+++ frontend/src/pages/travel_recommendation/TravelRecommendation.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Container } from "react-bootstrap";
+import styles from "../../styles/TravelRecommendation.module.css";
 
 export default function TravelRecommendation() {
   const currentUser = useCurrentUser();
   const [recommendation, setRecommendation] = useState();
+
   useEffect(() => {
     if (currentUser) {
       axios
@@ -19,22 +21,23 @@ export default function TravelRecommendation() {
     }
   }, [currentUser]);
 
-  const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-};
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
-    <div>
-        <h4>Places you may enjoy</h4>
-        {recommendation && recommendation.results.length > 0 ? (
-            <ListGroup horizontal='md'>
-                {recommendation.results[0].recommended_destination.map((destination, index) => (
-                    <ListGroup.Item key={index}>{capitalize(destination)}</ListGroup.Item>
-                ))}
-            </ListGroup>
-        ) : (
-            <p>No recommendations available.</p>
-        )}
-    </div>
-);
+    <Container className={styles.Container}>
+      <h4 className={styles.Heading}>Places You May Enjoy</h4>
+
+      {recommendation && recommendation.results.length > 0 ? (
+        <ListGroup horizontal="md" className={styles.ListContainer}>
+          {recommendation.results[0].recommended_destination.map((destination, index) => (
+            <ListGroup.Item key={index} className={styles.ListItem}>
+              {capitalize(destination)}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      ) : (
+        <p className={styles.NoData}>No recommendations available.</p>
+      )}
+    </Container>
+  );
 }
