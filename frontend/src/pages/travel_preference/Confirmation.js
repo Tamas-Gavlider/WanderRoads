2eafa243f12df7ff9e184f25aff33b03d972f502
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-18 14:06:52 +0100

    add style to the confirmation page

--- frontend/src/pages/travel_preference/Confirmation.js
+++ frontend/src/pages/travel_preference/Confirmation.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import btnStyles from '../../styles/Button.module.css';
+import styles from '../../styles/Confirmation.module.css';
 
 function Confirmation() {
   const currentUser = useCurrentUser(); 
   return (
-    <div className="text-center">
+    <div className={`text-center ${styles.Container}`}>
       <h2>Preferences successfully created!</h2>
       <p>Your travel preferences have been saved successfully.</p>
-      <Link to={`/profiles/${currentUser.profile_id}`} className="btn btn-primary">
+      <Link to={`/profiles/${currentUser.profile_id}`} className={`btn ${btnStyles.Button}`}>
         Go back to your profile
       </Link>
     </div>
