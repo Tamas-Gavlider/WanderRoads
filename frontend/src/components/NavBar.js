f6f681badbf6f55d9a19df02b77e1382a3813d9f
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-20 12:26:16 +0100

    change logo image to webp format/preload image on profile in index.html

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import {
