f5e8ca8b762bbf40c44b6dfbdfb4b5f52d1bcf4a
Tamas <6mv8g9c8@students.codeinstitute.net>
2025-03-28 14:16:36 +0100

    add spinner to avatar

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Avatar.module.css";
+import { Spinner } from "react-bootstrap"; // Import the Spinner component
 
 const Avatar = ({ src, height = 45, text }) => {
+  const [loading, setLoading] = useState(true); // State to manage loading status
+
+  const handleImageLoad = () => {
+    setLoading(false); // Set loading to false once the image is loaded
+  };
+
   return (
     <span>
+      {loading && <Spinner animation="border" variant="danger" />} 
       <img
         className={styles.Avatar}
         src={src}
         height={height}
         width={height}
         alt="avatar"
+        onLoad={handleImageLoad}
+        style={loading ? { visibility: "hidden" } : {}}
       />
       {text}
     </span>
